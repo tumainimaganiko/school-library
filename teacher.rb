e46e1da77dc1152983db613be94150b5597c8a43
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-23 21:24:45 +0300

    Update Teacher Class

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require "./person.rb"
 
 class Teacher < Person
-    def initialize (name = "unknown",parent_permission = true, age,id,specialization)
-        super(name,parent_permission,id,age)
+    def initialize (specialization)
+        super()
         @specialization = specialization
     end
 
