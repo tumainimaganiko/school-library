7d3a3621f0b7b6ff3748318370695d6dd24998f7
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-23 21:23:31 +0300

    Update Person Class

--- person.rb
+++ person.rb
@@ -1,4 +1,9 @@
 class Person
+
+  attr_reader :id
+  attr_accessor :name
+  attr_accessor :age
+
   def initialize (id,name = "unknown",age, parent_permission = true)
     @id = id
     @name = name
@@ -6,25 +11,13 @@ class Person
     @parent_permission = parent_permission  
   end
 
-  attr_reader :id
-  attr_reader :name
-  attr_reader :age
-
-  attr_writer :name
-  attr_writer :age
-
   def of_age?
-    if  @age >= 18
-        true
-    else
-        false
-    end
+    @age >= 18
   end
 
   private :of_age?
 
   def can_use_services?
-    if @age >= 18 || @parent_permission
-        true
-    end
+    @age >= 18 || @parent_permission == true
+  end
 end
