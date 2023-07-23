f83226eef9a7432b89c640799a7f157d421428b6
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-23 20:26:38 +0300

    Constructor

---
+++ person.rb
@@ -0,0 +1,8 @@
+class Person
+  def initialize (id,name = "unknown",age, parent_permission = true)
+    @id = id
+    @name = name
+    @age = age  
+    @parent_permission = parent_permission  
+  end
+end
