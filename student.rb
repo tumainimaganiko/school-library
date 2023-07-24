600da1641deb1b68acb20ab9c82b6efaab4ece42
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-24 22:34:00 +0300

    Modification

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require './person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_accessor :classroom
   
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
