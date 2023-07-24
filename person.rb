b53429bfaacec60a80917cd08db539eaf288673e
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-24 13:58:43 +0300

    Add Parents Constructor

--- person.rb
+++ person.rb
@@ -5,6 +5,7 @@ class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
