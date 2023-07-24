739c392f02d24398a45137268223e44490ff2c9f
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-24 21:20:58 +0300

    Create Relationship Class

--- rental.rb
+++ rental.rb
@@ -1,6 +1,13 @@
 class Rental
+    attr_reader :person, :book
     attr_accessor :date
-  def initialize(date)
-    @date = date    
+  def initialize(person, book, date)
+    @date = date 
+
+    @person = person
+    person.rentals << self
+
+    @book = book
+    book.rentals << self
   end
 end
