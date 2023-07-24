30c86fbfc5e040faaffc1df62f44743b40693e0e
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-24 21:25:53 +0300

    Update Student Class

--- student.rb
+++ student.rb
@@ -6,13 +6,8 @@ class Student < Person
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
-      
-    end
+  end
 
   def play_hooky
     '¯(ツ)/¯'
