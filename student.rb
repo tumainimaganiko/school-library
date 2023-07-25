f6afe21e41e536d1213743481fc4264f547bc1fa
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-25 08:50:19 +0300

    Belongs to Method

--- student.rb
+++ student.rb
@@ -6,6 +6,10 @@ class Student < Person
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 
