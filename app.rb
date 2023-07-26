1350a9d83fcaf0c7c53136195282f07dcca35af1
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-26 20:55:15 +0300

    Remove Display Method

--- app.rb
+++ app.rb
@@ -107,43 +107,4 @@ class App
     end
   end
 
-
-  def display
-
-    loop do
-      
-    puts "\nPlease choose an option by entering a number:"
-    puts "1 - List all books"
-    puts "2 - List all people"
-    puts "3 - Create a person"
-    puts "4 - Create a book"
-    puts "5 - Create a rental"
-    puts "6 - List all rentals for a given person id"
-    puts "7 - exit"
-
-    puts "Waiting for your option"
-    user_option = gets.chomp.to_i
-
-    case user_option
-    when 1
-      all_books
-    when 2
-      all_people
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      list_rentals
-    when 7
-      puts 'Thank you for using this app!'
-      break
-    else  
-      puts "Invalid Selection"
-    end
-
-    end
-  end
 end
