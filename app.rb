d3b3cf97130e5dedc4b673f3fb7eb26ef64226c3
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-26 20:39:13 +0300

    Create App Class

---
+++ app.rb
@@ -0,0 +1,15 @@
+require "./book.rb"
+require "./person.rb"
+require "./rental.rb"
+require "./student.rb"
+require "./teacher.rb"
+
+class App
+  def initialize
+    @books = []
+    @rentals = []
+    @people = []
+  end
+
+  
+end
