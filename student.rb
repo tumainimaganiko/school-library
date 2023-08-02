4b56cf2e7947fb77c7310c4ded6268b88e6724c2
Tumaini Maganiko <tumainimaganiko@gmail.com>
2023-08-02 12:08:41 +0300

    Update Array

--- student.rb
+++ student.rb
@@ -13,6 +13,19 @@ class Student < Person
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 
+  def as_json(options = {})
+    {
+      classroom: @classroom,
+      age: @age,
+      name: @name,
+      parent_permission: @parent_permission
+    }
+  end
+
+  def to_json(*options)
+    as_json(*options).to_json(*options)
+  end
+
   def play_hooky
     '¯(ツ)/¯'
   end
