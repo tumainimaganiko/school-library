45b79c05592b1e15a5b7b2ae8498d70828bfa157
Tumaini Maganiko <tumainimaganiko@gmail.com>
2023-08-02 12:07:44 +0300

    Add JSON format

--- createstudent.rb
+++ createstudent.rb
@@ -1,4 +1,5 @@
 require './student'
+require 'json'
 class CreateStudent
   def create_student(people)
     print "\nName: "
@@ -9,13 +10,15 @@ class CreateStudent
     parent_permission = gets.chomp
     if parent_permission.downcase == 'n'
       student = Student.new('classroom', age, true, name)
+      puts "Student created successfully: #{student.as_json}"
     elsif parent_permission.downcase == 'y'
       student = Student.new('classroom', age, false, name)
+      puts "Student created successfully: #{student.as_json}"
     else
       puts 'Invalid Selection for parent permission'
       return
     end
-    people << student
-    puts 'Student created succesfully'
+    people << student.to_json
+    puts "Student created succesfully: People = #{people}"
   end
 end
