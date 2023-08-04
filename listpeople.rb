2c81ef2022b6919440bca83ab4cf776c55a912c0
Tetteh Kodjo-Sarso <Tkodjosarso@gmail.com>
2023-08-04 07:40:10 +0000

    add listpeople method

--- listpeople.rb
+++ listpeople.rb
@@ -1,6 +1,12 @@
 class ListPeople
   def list_people(people)
     puts 'There are currently no recorded people in the system.' if people.empty?
-    people.each { |person| puts "\n[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+    people.each do |person|
+      if person.is_a?(Hash)
+        puts "\n[#{person['class']}] Name: #{person['name']}, ID: #{person['id']}, Age: #{person['age']}"
+      else
+        puts "\n[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+    end
   end
 end
