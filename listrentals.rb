f49be4021d80e673ce9a13f0d8e1f72701551c78
Tetteh Kodjo-Sarso <Tkodjosarso@gmail.com>
2023-08-04 07:38:52 +0000

    add data retrieving method

--- listrentals.rb
+++ listrentals.rb
@@ -3,15 +3,16 @@ class ListRentals
     print "\nID of the person: "
     person_id = gets.chomp.to_i
 
-    found_rentals = rentals.select { |rental| rental.person.id == person_id }
-
-    if found_rentals.empty?
-      puts "There are currently no rented books in the system under #{person_id} id."
-    else
-      puts 'Rentals:'
-      found_rentals.each do |rental|
-        puts "Date: #{rental.date}, Book: \"#{rental.book.title}\" by #{rental.book.author}"
+    rentals.each do |values|
+      if values.is_a?(Hash)
+        if values['id'].to_i == person_id.to_i
+          return puts "Date: #{values['date']}, Book: \"#{values['title']}\" by #{values['author']}"
+        end
+      elsif values.person.id == person_id
+        return puts "Date: #{values.date}, Book: \"#{values.book.title}\" by #{values.book.author}"
       end
     end
+
+    puts "There are currently no rented books in the system under #{person_id} id."
   end
 end
