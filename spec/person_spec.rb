7408f81b4360814f18be1e7da2603d43d2594782
Tumaini Maganiko <tumainimaganiko@gmail.com>
2023-08-04 11:44:05 +0300

    Add test for Person Class

---
+++ spec/person_spec.rb
@@ -0,0 +1,25 @@
+require './person'
+require './book'
+require 'date'
+
+describe Person do
+  let(:person) { Person.new(20, 'John Doe', parent_permission: true) }
+  let(:book) { Book.new('The Great Gatsby', 'F. Scott Fitzgerald') }
+  let(:rental_date) { Date.today }
+
+  describe '#initialize' do
+    it 'initializes a person with the provided age' do
+      expect(person.age).to eq(20)
+    end
+
+    it "initializes a person with the provided name or 'unknown'" do
+      expect(person.name).to eq('John Doe')
+    end
+  end
+
+  describe '#correct_name' do
+    it 'returns the correct name of the person' do
+      expect(person.correct_name).to eq('John Doe')
+    end
+  end
+end
